bf60f056b0f790e29f7c9c0c52e37a74b481daa9
pweingar <pweingar@gmail.com>
2024-10-31 10:31:39 -0400

    Fixed linker error in Calypsi hello example.

--- calypsi/hello/f256-plain.scm
+++ calypsi/hello/f256-plain.scm
@@ -1,16 +1,20 @@
 (define memories
-  '(
-	(memory LoMem
-  		(address (#xa000 . #xcfff))
-		(type ANY))
+  '((memory LoMem
+      (address (#xa000 . #xcfff))
+      (type ANY))
+
     (memory Vector (address (#xffe4 . #xffff)))
+
     (memory Banks
-		(address (#x10000 . #x1ffff))
-		(type ANY)
-		(section (header #x10000)))
+      (address (#x10000 . #x1ffff))
+      (type ANY)
+      (section (header #x10000))
+      (placement-group far-bits (section ANY))
+      (placement-group far-nobits (section zfar zdata)))
+
     (memory DirectPage
-		(address (#x000100 . #x0001ff))
-        (section (registers ztiny)))
+      (address (#x000100 . #x0001ff))
+          (section (registers ztiny)))
 
     (block stack (size #x1000))
     (block heap (size #x1000))
